a672d534fc084ced224d720a52e980d4d07c5f57
katyratka <kratka@axialhealthcare.com>
2021-02-02 14:06:55 -0600

    Adds potential SQL injection

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def get_parks(request):
+    from parks.models import Park
+    name = request.GET.get("name")
+    raw = Park.objects.raw(f'SELECT * FROM playground_parks WHERE name = {name}')
     return render(request, 'parks.html', {})
