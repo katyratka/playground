8100d1afa072a083cd44f67ab60cb02ee95f8c15
katyratka <kratka@axialhealthcare.com>
2021-02-02 14:20:18 -0600

    Updates with create

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 def get_parks(request):
     from parks.models import Park
     name = request.GET.get("name")
-    raw = Park.objects.raw(f'SELECT * FROM playground_parks WHERE name = {name}')
+    # raw = Park.objects.raw(f'SELECT * FROM playground_parks WHERE name = {name}')
+
+    Park.objects.get_or_create(
+        name=name,
+        description=name
+    )
     return render(request, 'parks.html', {})
