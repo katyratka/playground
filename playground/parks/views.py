c7248c81b3b22586c6102486ce053684faec9915
katyratka <kratka@axialhealthcare.com>
2021-02-04 15:53:17 -0600

    raw sql

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -1,12 +1,17 @@
+from django import db
 from django.shortcuts import render
+
 
 def get_parks(request):
     from parks.models import Park
     name = request.GET.get("name")
-    # raw = Park.objects.raw(f'SELECT * FROM playground_parks WHERE name = {name}')
+    raw = Park.objects.raw(f'SELECT * FROM playground_parks WHERE name = {name}')
 
-    Park.objects.get_or_create(
-        name=name,
-        description=name
-    )
+    cursor = db.cursor()
+    cursor.execute(raw)
+
+    # Park.objects.get_or_create(
+    #     name=name,
+    #     description=name
+    # )
     return render(request, 'parks.html', {})
