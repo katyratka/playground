0361484ffce9d21f1a70deaeba4144767bac86c8
katyratka <kratka@axialhealthcare.com>
2021-02-05 14:46:39 -0600

    Update the naming

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def get_parks(request):
 
     tmp_dir = os.environ.get('TMPDIR')
-    file = open(tmp_dir+"/temporary_file","w+")
+    file = open(tmp_dir+"/tmp","w+")
 
     return render(request, 'parks.html', {})
