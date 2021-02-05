bc8e176de1c84ef6972b1617f24bb3c9b8a5015c
katyratka <kratka@axialhealthcare.com>
2021-02-05 09:58:59 -0600

    Raw SQL

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -1,17 +1,17 @@
 from django import db
 from django.shortcuts import render
+from django.db.models.expressions import RawSQL
 
 
 def get_parks(request):
-    from parks.models import Park
     name = request.GET.get("name")
-    raw = Park.objects.raw(f'SELECT * FROM playground_parks WHERE name = {name}')
+    raw = f'SELECT * FROM playground_parks WHERE name = {name}'
 
+    # Option 1 for bad SQL injection
     cursor = db.cursor()
     cursor.execute(raw)
 
-    # Park.objects.get_or_create(
-    #     name=name,
-    #     description=name
-    # )
+    # Option 2 for bad SQL injection
+    test = RawSQL(raw)
+
     return render(request, 'parks.html', {})
