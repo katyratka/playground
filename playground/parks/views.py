faaaea050d0c2e540a489522e1e3f4d0da67d840
katyratka <kratka@axialhealthcare.com>
2021-02-05 14:29:26 -0600

    writable directory

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -1,4 +1,9 @@
+import os
 from django.shortcuts import render
 
 def get_parks(request):
+
+    tmp_dir = os.environ.get('TMPDIR')
+    file = open(tmp_dir+"/temporary_file","w+")
+
     return render(request, 'parks.html', {})
