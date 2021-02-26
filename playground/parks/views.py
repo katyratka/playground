5f4d020f2f9507980f002ef369b1dc779e4a4ccb
katyratka <kratka@axialhealthcare.com>
2021-02-26 14:36:01 -0600

    Updates the hardcoded tmp file path

--- playground/parks/views.py
+++ playground/parks/views.py
@@ -1,9 +1,11 @@
 import os
+import tempfile
 from django.shortcuts import render
 
 def get_parks(request):
 
-    tmp_dir = os.environ.get('TMPDIR')
-    file = open(tmp_dir+"/tmp","w+")
+    file = tempfile.TemporaryFile(
+        dir="/tmp/my_subdirectory", mode='"w+"
+    )
 
     return render(request, 'parks.html', {})
